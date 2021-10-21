a0cd8273155dd79d6f2d181658a289c6af1957db
sangeethaNR <sangeethajadhav123@gmail.com>
2021-10-21 11:00:48 -0700

    service worker file updated

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,9 @@
 const FILES_TO_CACHE = [
   "/",
-  "index.html", 
-  "style.css",
-  "index.js",
-  "db.js",
+  "/index.html", 
+  "/styles.css",
+  "/index.js",
+  "/db.js",
   "/manifest.webmanifest",
     "/icons/icon-192x192.png",
   "/icons/icon-512x512.png"
